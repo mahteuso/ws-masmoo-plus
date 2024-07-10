be0c34cd3c048ee7f88bf2470e134230fb0f7711
mahteuso <mateus@ufscar.br>
2024-07-10 10:26:02 -0300

    Config: starting project with gitignore, pom.xml and src.

---
+++ src/main/java/com/client/ws/masmooplus/WsMasmooPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.masmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMasmooPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMasmooPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/ws/masmooplus/WsMasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ws.masmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
